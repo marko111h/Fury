82394e7cb82b0511bd5457a53534ef00b6b81294
Aleksandar <003aca73@gmail.com>
2024-04-17 11:55:52 +0200

    Added basic pathfinding

--- scripts/grid.py
+++ scripts/grid.py
@@ -1,7 +1,4 @@
-class Vec2:
-    def __init__(self, q: int, r: int):
-        self.q = q
-        self.r = r
+from typing import Tuple
 
 class Grid:
     def __init__(self, radius: int):
@@ -9,7 +6,7 @@ class Grid:
         self.__partSize = radius * (radius + 1)
         self.__list = [None] * (self.__partSize * 3 + 1)
 
-    def __index_function(self, q: int, r: int) -> int:
+    def index_function(self, q: int, r: int) -> int:
         x: int = q
         y: int = r
         z: int = Grid.get_z(q, r)
@@ -22,10 +19,10 @@ class Grid:
         return 0
 
     def get(self, q: int, r: int):
-        return self.__list[self.__index_function(q, r)]
+        return self.__list[self.index_function(q, r)]
 
     def set(self, q: int, r: int, value):
-        self.__list[self.__index_function(q, r)] = value
+        self.__list[self.index_function(q, r)] = value
 
     def get_size(self):
         return self.__radius
@@ -35,21 +32,21 @@ class Grid:
         return -q - r
 
     @staticmethod
-    def get_dist(a: Vec2, b: Vec2):
-        return (abs(a.q - b.q) + abs(a.q + a.r - b.q - b.r) + abs(a.r - b.r)) / 2
+    def get_dist(a: Tuple[int, int], b: Tuple[int, int]):
+        return (abs(a[0] - b[0]) + abs(a[0] + a[1] - b[0] - b[1]) + abs(a[1] - b[1])) / 2
 
     @staticmethod
-    def get_point_south(a: Vec2, dist: int) -> Vec2:
-        return Vec2(a.q, a.r + dist)
+    def get_point_south(a: Tuple[int, int], dist: int) -> Tuple[int, int]:
+        return a[0], a[1] + dist
 
     @staticmethod
-    def get_point_south_east(a: Vec2, dist: int) -> Vec2:
-        return Vec2(a.q + dist, a.r)
+    def get_point_south_east(a: Tuple[int, int], dist: int) -> Tuple[int, int]:
+        return a[0] + dist, a[1]
 
     @staticmethod
-    def get_point_south_west(a: Vec2, dist: int) -> Vec2:
-        return Vec2(a.q - dist, a.r + dist)
+    def get_point_south_west(a: Tuple[int, int], dist: int) -> Tuple[int, int]:
+        return a[0] - dist, a[1] + dist
 
     @staticmethod
-    def is_reachable(self, a: Vec2, b: Vec2) -> bool:
-        return True #TODO: Implement
+    def is_reachable(self, a: Tuple[int, int], b: Tuple[int, int]) -> bool:
+        return True  # TODO: Implement
