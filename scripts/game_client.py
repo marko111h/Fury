3fcf79feb667c70f6b75f053254526aca7bb6486
KonstantinosTrimikliniotis <kostenka177@yandex.ru>
2024-04-20 14:35:12 +0300

    style fixes, code refactoring

--- scripts/game_client.py
+++ scripts/game_client.py
@@ -14,7 +14,6 @@ class GameClient:
     """ Class is responsible to provide game logic and
         keep up to date the state of the game for client
     """
-    server_address = ("wgforge-srv.wargaming.net", 443)
 
     class Result(Enum):
         OKEY = 0
@@ -39,7 +38,7 @@ class GameClient:
     class Action:
         def __init__(self, action_type: 'GameClient.ActionType', player: Player, vehicle: Tank,
                      target: Tuple[int, int] = None):
-            self.type: 'GameClient.ActionType' = action_type
+            self.type: GameClient.ActionType = action_type
             self.player: Player = player
             self.vehicle: Tank = vehicle
             self.target: Optional[Tuple[int, int]] = target
@@ -66,8 +65,13 @@ class GameClient:
         self.update_map()
 
     @staticmethod
-    def __send_request__(client_socket: socket, action: 'GameClient.ActionType', json_data: str = "") -> None:
-        request: bytes = (action.value.to_bytes(4, byteorder="little")
+    def __send_request__(client_socket: socket, action_type: 'GameClient.ActionType', json_data: str = "") -> None:
+        """ Sends request to the server
+        :param client_socket: socket to be used
+        :param action_type: type of the action
+        :param json_data: additional data as json
+        """
+        request: bytes = (action_type.value.to_bytes(4, byteorder="little")
                           + len(json_data).to_bytes(4, byteorder="little"))
         if json_data != "":
             request += json_data.encode('utf-8')
@@ -75,10 +79,13 @@ class GameClient:
         client_socket.sendall(request)
 
     @staticmethod
-    def __process_response__(client_socket: socket) -> Tuple['GameClient.Result', Optional[Dict]]:
+    def __get_response__(client_socket: socket) -> Tuple['GameClient.Result', Optional[Dict]]:
+        """ Gets response from the server
+        :param client_socket:
+        :return: Tuple of two: 1) Result of the action, 2) Additional data as dictionary (parsed from json)
+        """
         # Receiving result code as bytes
         result_code: bytes = client_socket.recv(4)
-
         # Converting bytes into Enum
         result_enum: GameClient.Result = GameClient.Result(
             int.from_bytes(result_code, byteorder="little")
@@ -86,9 +93,10 @@ class GameClient:
         print("Result of request:", result_enum)
         # Read the length of JSON with data
         json_data_length: int = int.from_bytes(client_socket.recv(4), byteorder="little")
-        if result_enum != GameClient.Result.OKEY or json_data_length == 0:
+        # Check if there is additional data
+        if json_data_length == 0:
             return result_enum, None
-
+        # Read additional data
         json_bytes: bytes = client_socket.recv(json_data_length)
         json_data: Dict = json.loads(json_bytes.decode('utf-8'))
         return result_enum, json_data
@@ -118,7 +126,7 @@ class GameClient:
         # copies dictionary of names and values
         # for all local variables (in our case parameters)
         params = locals().copy()
-        # because it is classmethod we need to remove first parameter
+        # removes parameters not used for server request
         params.pop("cls")
         params.pop("client_socket")
         # removes every None from calls like: login("Boris", password = None)
@@ -126,14 +134,11 @@ class GameClient:
                            if value is not None}
         # separators to remove spaces between key and value
         json_params: str = json.dumps(filtered_params, separators=(',', ':'))
-        client_socket: socket = None
         try:
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect(GameClient.server_address)
             # sends login request to the server
             GameClient.__send_request__(client_socket, GameClient.ActionType.LOGIN, json_params)
             # gets response from the server
-            result_enum, json_data = GameClient.__process_response__(client_socket)
+            result_enum, json_data = GameClient.__get_response__(client_socket)
 
             if result_enum != GameClient.Result.OKEY:
                 return None
@@ -141,59 +146,68 @@ class GameClient:
             player_id: int = json_data["idx"]
             player_name: str = json_data["name"]
             is_observer: bool = json_data["is_observer"]
-            # TODO: create game with provided through args settings
+            # TODO: save settings which are not visible through game_state request (is_full mb)
             return GameClient(player_id, client_socket)
         except socket.error as e:
             print(f"Socket error: {e}")
             return None
 
     def logout(self) -> None:
-        """ Logouts the player from the game and closes the socket
+        """ Logouts the player from the game
         """
         try:
             GameClient.__send_request__(self.__client_socket, GameClient.ActionType.LOGOUT)
-            result_enum, _ = GameClient.__process_response__(self.__client_socket)
+            result_enum, _ = GameClient.__get_response__(self.__client_socket)
             print("Logout result:", result_enum.name)
         except socket.error as e:
             print(f"Socket error: {e}")
+
+    def __update_tanks__(self, json_vehicles_data: Dict[str, Any]):
+        # key: vehicle id, value: vehicle features as dictionary
+        for vehicle_id, vehicle_features in json_vehicles_data.items():
+            owner: Player = self.__find_player__(vehicle_features["player_id"])
+            if not owner:
+                print("No vehicle owner found error!")
+                raise ValueError
+            cur_pos: Tuple[int, int] = (vehicle_features["position"]["x"], vehicle_features["position"]["y"])
+            spawn_pos: Tuple[int, int] = (vehicle_features["spawn_position"]["x"],
+                                          vehicle_features["spawn_position"]["y"])
+            vehicle_type: str = vehicle_features["vehicle_type"]
+            # TODO: only medium tanks now
+            if vehicle_type == "medium_tank":
+                vehicle: Tank = MediumTank(vehicle_id, cur_pos, spawn_pos, vehicle_features["capture_points"],
+                                           owner, None, 1)
+                owner.add_tank(vehicle)
 
     def update_game_state(self) -> None:
         """ Updates the state of the game by requesting GAME_STATE
         """
         try:
+            # GAME_STATE request to the server
             GameClient.__send_request__(self.__client_socket, GameClient.ActionType.GAME_STATE)
-            result_enum, json_game_state = GameClient.__process_response__(self.__client_socket)
+            # get the response
+            result_enum, json_game_state = GameClient.__get_response__(self.__client_socket)
+            if result_enum != GameClient.Result.OKEY:
+                # error msg
+                print(json_game_state)
+                return
+            # updating the data based on json_game_state
+            self.__turns = json_game_state["num_turns"]
+            self.__rounds = json_game_state["num_rounds"]
+            self.__cur_turn = json_game_state["current_turn"]
+            self.__cur_round = json_game_state["current_round"]
+            self.__round_finished = json_game_state["finished"]
 
-            if result_enum == GameClient.Result.OKEY:
-                self.__turns = json_game_state["num_turns"]
-                self.__rounds = json_game_state["num_rounds"]
-                self.__cur_turn = json_game_state["current_turn"]
-                self.__cur_round = json_game_state["current_round"]
+            # clears players list in order to update with server data
+            self.__players.clear()
+            for player in json_game_state["players"]:
+                # TODO: creates only RealPlayer, need default player for opponents
+                self.__players.append(RealPlayer(player["idx"], player["name"], player["is_observer"]))
 
-                # clears players list in order to update with server data
-                self.__players.clear()
-                for player in json_game_state["players"]:
-                    # TODO: creates only BOTs, need default player for opponents
-                    self.__players.append(RealPlayer(player["idx"], player["name"], player["is_observer"]))
-                self.__round_finished = json_game_state["finished"]
-                vehicles = json_game_state["vehicles"]
-                # key: vehicle id, value: vehicle features as dictionary
-                for vehicle_id, vehicle_features in vehicles.items():
-                    owner: Player = self.__find_player__(vehicle_features["player_id"])
-                    if not owner:
-                        print("No vehicle owner found error!")
-                        raise ValueError
-                    cur_pos: Tuple[int, int] = (vehicle_features["position"]["x"], vehicle_features["position"]["y"])
-                    spawn_pos: Tuple[int, int] = (vehicle_features["spawn_position"]["x"],
-                                                  vehicle_features["spawn_position"]["y"])
-                    vehicle_type: str = vehicle_features["vehicle_type"]
-                    # TODO: only medium tanks now
-                    if vehicle_type == "medium_tank":
-                        vehicle: Tank = MediumTank(vehicle_id, cur_pos, spawn_pos, vehicle_features["capture_points"],
-                                                   owner, None, 1)
-                        owner.add_tank(vehicle)
-                # TODO: order players correctly, finished, attack matrix, winner, win_points
-                # TODO: player_result_points, catapult_usage
+            json_vehicles_data = json_game_state["vehicles"]
+            self.__update_tanks__(json_vehicles_data)
+            # TODO: order players correctly, finished, attack matrix, winner, win_points
+            # TODO: player_result_points, catapult_usage
             winner_id: Optional[int] = json_game_state["winner"]
             if winner_id:
                 self.__winner = self.__find_player__(winner_id)
@@ -205,37 +219,45 @@ class GameClient:
         """
         try:
             GameClient.__send_request__(self.__client_socket, GameClient.ActionType.MAP)
-            result_enum, json_data = GameClient.__process_response__(self.__client_socket)
+            result_enum, json_data = GameClient.__get_response__(self.__client_socket)
             if result_enum == GameClient.Result.OKEY:
                 self.__grid: Grid = Grid(json_data["size"])
                 # TODO: spawns
                 map_content: Dict[str, Any] = json_data["content"]
                 base: List[Dict[str, int]] = map_content["base"]
-                # TODO: fix KeyError: 'obstacle'
-                # obstacles: List[Dict[str, int]] = map_content["obstacle"]
                 self.__base.clear()
                 for base_cell in base:
                     self.__base.append((base_cell["x"], base_cell["y"]))
+                # TODO: fix KeyError: 'obstacle'
+                # obstacles: List[Dict[str, int]] = map_content["obstacle"]
                 # self.__obstacles.clear()
                 # for obstacle_cell in obstacles:
                 #     self.__obstacles.append((obstacle_cell["x"], obstacle_cell["y"]))
         except socket.error as e:
             print(f"Socket error: {e}")
 
-    def get_game_actions(self) -> List['GameClient.Action']:
+    def get_game_actions(self) -> Optional[List['GameClient.Action']]:
         """ Requests game actions from the server and returns them
         @:return returns list of actions got from server
         """
         GameClient.__send_request__(self.__client_socket, GameClient.ActionType.GAME_ACTIONS)
-        result_enum, json_data = GameClient.__process_response__(self.__client_socket)
-        actions: List['GameClient.Action'] = []
-        if result_enum == GameClient.Result.OKEY:
-            for action in json_data["actions"]:
-                player: Optional[RealPlayer] = self.__find_player__(action["player_id"])
-                action_type: GameClient.ActionType = GameClient.ActionType(action["action_type"])
-                action_data: str = action["data"]
-                # TODO parse action data
-                action.append(GameClient.Action(action_type, player, action_data))
+        result_enum, json_data = GameClient.__get_response__(self.__client_socket)
+        actions: List[GameClient.Action] = []
+        if result_enum != GameClient.Result.OKEY:
+            # print error code
+            print(json_data)
+        for action in json_data["actions"]:
+            player: Optional[RealPlayer] = self.__find_player__(action["player_id"])
+            action_type: GameClient.ActionType = GameClient.ActionType(action["action_type"])
+            action_data: Dict[str, Any] = action["data"]
+            vehicle: Optional[Tank] = None
+            for vehicle_ in player.tanks:
+                if vehicle_.id == action_data["vehicle_id"]:
+                    vehicle = vehicle_
+            if not vehicle:
+                raise ValueError
+            target: Tuple[int, int] = action_data["target"]["x"], action_data["target"]["y"]
+            action.append(GameClient.Action(action_type, player, vehicle, target))
         return actions
 
     def __get_turn__(self) -> Action:
@@ -288,11 +310,15 @@ class GameClient:
         return GameClient.Action(type_, player, vehicle, (x, y))
 
     def send_turn_end(self) -> None:
+        """ Send TURN request to the server
+        """
         GameClient.__send_request__(self.__client_socket, GameClient.ActionType.TURN)
-        result_enum, _ = GameClient.__process_response__(self.__client_socket)
+        result_enum, _ = GameClient.__get_response__(self.__client_socket)
         print("End of turn send", result_enum.name)
 
-    def play_turn(self):
+    def play_turn(self) -> None:
+        """ Plays turn from client
+        """
         action: GameClient.Action = self.__get_turn__()
         if not action.target:
             self.send_turn_end()
@@ -300,6 +326,8 @@ class GameClient:
         # I don't know if we have to do that in client logic
         # self.__execute_action__(action)
         # send the action to the server
+
+        # make dict for future json
         action_data_dict: Dict[str, Any] = {
             "vehicle_id": action.vehicle.id,
             "target": {
@@ -310,7 +338,7 @@ class GameClient:
         }
         action_data_json: str = json.dumps(action_data_dict, separators=(',', ':'))
         GameClient.__send_request__(self.__client_socket, action.type, action_data_json)
-        result_enum, _ = GameClient.__process_response__(self.__client_socket)
+        result_enum, _ = GameClient.__get_response__(self.__client_socket)
         print("Move result", result_enum.name)
         if result_enum == GameClient.Result.OKEY:
             self.send_turn_end()
@@ -352,7 +380,9 @@ class GameClient:
         assert (action.type == GameClient.ActionType.CHAT)
         pass
 
-    def update_turn(self):
+    def update_turn(self) -> None:
+        """ Increases cur_turn by 1
+        """
         self.__cur_turn += 1
 
     def round_finished(self):
